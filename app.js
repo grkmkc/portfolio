28837593af5f6d2a07ec79b21c9e3483f911f0c1
unknown <gorkemkoc.tr@gmail.com>
2018-01-12 11:29:33 +0300

    new images and style

--- app.js
+++ app.js
@@ -12,4 +12,32 @@ $(document).ready(function () {
             easing: "easeInOutQuad",
             duration: 4000
         });
+        $('.one').each(function () {
+            $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+        });
+
+        anime.timeline({
+                loop: false
+            })
+            .add({
+                targets: '.one',
+                opacity: [0, 1],
+                easing: "easeInOutQuad",
+                duration: 4000
+            });
+            $('.anime').each(function () {
+                $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+            });
+
+            anime.timeline({
+                    loop: false
+                })
+                .add({
+                    targets: '.anime',
+                    opacity: [0, 1],
+                    easing: "easeInOutQuad",
+                    duration: 4000
+                });
 });
+
+
